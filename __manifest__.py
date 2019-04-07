57539c2230ba7ff39a4d6d0aa3a75a9bdad56349
adham95 <adham.elsabbagh@gmail.com>
2019-04-07 17:25:40 +0200

    hr workflow last update

--- __manifest__.py
+++ __manifest__.py
@@ -1,37 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "exit_reenter",
+    'name': "Exit Reenter",
 
-    'summary': """
+    'summary': """ this module for request exit entry for saudi hr systems 
         """,
-
-    'description': """
-    """,
 
     'author': "Raed Technology",
     'website': "http://www.raedtechnology.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
-    # for the full list
     'category': 'hr',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base','common_base','hr_contract_allded','hr'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'security/security.xml',
         'views/views.xml',
-        'views/templates.xml',
-        'security/security.xml',
         'security/ir.model.access.csv',
 
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
 }
